5827d024204d61497f9c40d5fd739f7734b06c73
BorisovMaksim <maksim.borisov.2013@gmail.com>
2022-05-05 18:32:17 +0300

    parsed_audio_features

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import convert_data
-import load_data_to_database
 from sqlalchemy import create_engine
 import pandas as pd
 from get_songs import download_songs
 from myconstants import *
+from load_data_from_spotify_api import get_audio_features
 
 
 
@@ -14,9 +14,13 @@ def main():
                            .format(CREDENTIALS['maksim']['password']))
     if DATA_IS_NOT_CONVERTED:
         convert_data.make_csv_files_from_json_files()
+    if AUDIO_FEATURES_ARE_NOT_PARSED:
+        audio_features = get_audio_features(track_path='track_full.csv', step=50)
+        audio_features.to_sql('track', con=engine, if_exists='replace', index=False, chunksize=5)
     if TABLES_ARE_NOT_CREATED:
-        for filename in ['playlist_full.csv', 'track_full.csv', 'playlist_track_full.csv']:
-            load_data_to_database.csv_table_to_sql(engine, SPOTIFY_DATA_PATH, filename)
+        for filename in ['playlist_full.csv', 'playlist_track_full.csv']:
+            df = pd.read_csv(SPOTIFY_DATA_PATH + filename)
+            df.to_sql(filename[:-9], con=engine, if_exists='replace', index=False, chunksize=5)
     if SONGS_ARE_NOT_DOWNLOADED:
         playlist_track_random = pd.read_sql_query('WITH random_pid AS '
                                                   '(select pid from playlist order by random() limit 100) '
