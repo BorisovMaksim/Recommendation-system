1b30b861fc9346828c3802223d0ff69dc173a3a2
BorisovMaksim <maksim.borisov.2013@gmail.com>
2022-06-13 21:01:32 +0300

    train

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from app import App
 
 
+
 def main(model_name, tracks_uri, n):
     system = App(model_name=model_name)
     system.collect_data()
@@ -12,8 +13,8 @@ def main(model_name, tracks_uri, n):
 
 
 if __name__ == '__main__':
-    main("annoy", tracks_uri=["spotify:track:5yVb28fGlg5INf581ihyhl",
-                              "spotify:track:3VdRAvHOqPhWFIbCrW15Pe",
-                              "spotify:track:5ec7ipz0tV2oNzWnAtR53G",
-                              "spotify:track:2BTnLcf4QA05mB0VpXdCLw",
-                              "spotify:track:3GLklY1nu9hS4JebAA6hS3"], n=10)
+    main(model_name="annoy", tracks_uri=["spotify:track:5yVb28fGlg5INf581ihyhl",
+                                         "spotify:track:3VdRAvHOqPhWFIbCrW15Pe",
+                                         "spotify:track:5ec7ipz0tV2oNzWnAtR53G",
+                                         "spotify:track:2BTnLcf4QA05mB0VpXdCLw",
+                                         "spotify:track:3GLklY1nu9hS4JebAA6hS3"], n=10)
