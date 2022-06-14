3eab2632e53ab104e7fa0cf954dc5cd1662f3671
BorisovMaksim <maksim.borisov.2013@gmail.com>
2022-06-14 22:15:44 +0300

    api

--- playlist.py
+++ playlist.py
@@ -1,11 +1,1 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
-
-
-class Playlist(Resource):
-    def get(self, tracks):
-        for quote in ai_quotes:
-            if(quote["id"] == id):
-                return quote, 200
-        return "Quote not found", 404
 
