c30bd47598672f0f32404cafcf2bd5d1b01c3ddf
BorisovMaksim <maksim.borisov.2013@gmail.com>
2022-06-16 21:57:58 +0300

    fix

--- main.py
+++ main.py
@@ -1,35 +1,18 @@
 from app import App
-import json
-from flask import Flask
-from flask_restful import reqparse, Api, Resource
-
-app = Flask(__name__)
-api = Api(app)
-
-parser = reqparse.RequestParser()
-parser.add_argument('tracks', type=list, location='form')
-parser.add_argument('n', type=int, location='form')
-
-system = App(model_name='annoy')
-system.collect_data()
-system.extract_features()
-system.train()
 
 
-class Playlist(Resource):
-    def get(self):
-        return """You can use POST for track recommendation! For example 
-        curl http://localhost:5000/recommend -d 'n=5&
-        tracks=["spotify:track:7EDxhByqnzl1B4VJfMXZf4",  "spotify:track:3f0MU7LerrhPX2sJxkGyNa"]' -X POST -v"""
-
-    def post(self):
-
-        args = parser.parse_args()
-        tracks_uri = json.loads("".join(args['tracks']))
-        similar_tracks = system.model.predict(tracks_uri=tracks_uri, n=args['n'])
-        return similar_tracks.to_json()
+def main(tracks_uri, n):
+    system = App(model_name='annoy')
+    system.collect_data()
+    system.extract_features()
+    system.train()
+    similar_tracks = system.model.predict(tracks_uri=tracks_uri, n=n)
+    return similar_tracks
 
 
-api.add_resource(Playlist, '/recommend')
 if __name__ == '__main__':
-    app.run()
+    main(tracks_uri=["spotify:track:7EDxhByqnzl1B4VJfMXZf4",
+                     "spotify:track:3f0MU7LerrhPX2sJxkGyNa",
+                     "spotify:track:7I10ft1XHvVcYMJGNcevU8",
+                     "spotify:track:4ezv28TGOKCdxjDWPSwVf6",
+                     "spotify:track:6Wv7WQ9ymnLR6g40Cuud6v"], n=3)
